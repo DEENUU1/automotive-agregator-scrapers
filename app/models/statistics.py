0d004c6b3580facfb6f509c5c463297af698ff64
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-07-06 01:55:04 +0200

    feature: add ScraperStatistic and ParserStatistic models

---
+++ app/models/statistics.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, BaseConfig, Field
+from models.pyobject_id import PyObjectId
+from bson import ObjectId
+
+
+class ScraperStatistic(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
+    run_date: str
+    end_date: str
+    total_time: float
+    visited_pages: int
+    scraper_name: str
+
+    class Config(BaseConfig):
+        json_encoders = {ObjectId: str}
+        allow_population_by_field_name = True
+
+
+class ParserStatistic(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
+    run_date: str
+    end_date: str
+    input_elements: int
+    parsed_elements: int
+    saved_elements: int
+    parser_name: str
+
+    class Config(BaseConfig):
+        json_encoders = {ObjectId: str}
+        allow_population_by_field_name = True
